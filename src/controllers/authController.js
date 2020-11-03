4df50b9ae1375a0f04e3e249ae45f630d0229b9a
Maciel Galeano <macielgaleano.jh@gmail.com>
2020-11-03 14:52:28 -0300

    new user

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,8 +1,22 @@
 const { Router } = require("express");
+const User = require("../models/User");
 const router = Router();
+const USer = require("../models/User");
 
-router.post("/signUp", (req, res, next) => {});
-router.post("/signin", (req, res, next) => {});
-router.post("/me", (req, res, next) => {});
+router.post("/signup", (req, res, next) => {
+  const { username, email, password } = req.body;
+  const user = new User({
+    username: username,
+    email: email,
+    password: password,
+  });
+  res.json(user);
+});
+router.post("/signin", (req, res, next) => {
+  res.json("signin");
+});
+router.get("/me", (req, res, next) => {
+  res.json("me");
+});
 
 module.exports = router;
