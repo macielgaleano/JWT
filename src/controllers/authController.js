7e2037dceaac5e6517ca517d0d6bcc26e4d303d4
Maciel Galeano <macielgaleano.jh@gmail.com>
2020-11-03 14:38:09 -0300

    Auth controller created

---
+++ src/controllers/authController.js
@@ -0,0 +1,8 @@
+const { Router } = require("express");
+const router = Router();
+
+router.post("/signUp", (req, res, next) => {});
+router.post("/signin", (req, res, next) => {});
+router.post("/me", (req, res, next) => {});
+
+module.exports = router;
